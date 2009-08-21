a9ccf8f066adf7bc9152ca4d831e4cf5faffa4bd
Bill Pemberton <wfp5p@virginia.edu>
2009-08-21 09:31:26 -0400

    move variable defines around in breadcrumb
    This makes the byte-compiler happy

--- emacs.d/breadcrumb.el
+++ emacs.d/breadcrumb.el
@@ -168,8 +168,6 @@
 
 
 
-;;; Code:
-;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -199,6 +197,43 @@
   :type 'boolean
   :group 'breadcrumb)
 
+;;; Program global variables:
+
+(defvar *bc-bookmarks* ()
+  "List of bookmarks and their records.
+The list is (Bookmark1 Bookmark2 ...) where each Bookmark is (TYPE FILENAME . POSITION)"
+  )
+
+(defvar *bc-current* 0
+  "The current bookmark.  `bc-next' and `bc-previous' would use this as the starting point."
+  )
+
+(defvar *bc-bookmark-just-added* nil
+  "Flag indicates a bookmark has just been added.  `bc-next' and `bc-previous' use this to determine whether to increment or decrement."
+  )
+
+;; these come in from info.el
+;; these defines are to keep the compiler happy
+(defvar Info-current-file) ;; from info.el
+(defvar Info-current-node) ;; from info.el
+(declare-function Info-find-node "info" (filename nodename
+						  &optional no-going-back))
+
+
+;;; Buffer type constants
+
+(defconst bc--type-unsupported  'unsupported)
+(defconst bc--type-file         'file)
+(defconst bc--type-dired        'dired)
+(defconst bc--type-info         'info)
+(defconst bc--type-system       'system)
+
+;;; Constants
+(defconst bc--menu-buffer       "*Breadcrumb Bookmarks*")
+(defconst bc--file-magic        "WBC")
+(defconst bc--file-version      1)
+
+(defconst bc--menu-table-offset 7)
 
 ;;; User callable functions
 
@@ -287,34 +322,6 @@
 ;; Private section
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-;;; Program global variables:
-
-(defvar *bc-bookmarks* ()
-  "List of bookmarks and their records.
-The list is (Bookmark1 Bookmark2 ...) where each Bookmark is (TYPE FILENAME . POSITION)"
-  )
-
-(defvar *bc-current* 0
-  "The current bookmark.  `bc-next' and `bc-previous' would use this as the starting point."
-  )
-
-(defvar *bc-bookmark-just-added* nil
-  "Flag indicates a bookmark has just been added.  `bc-next' and `bc-previous' use this to determine whether to increment or decrement."
-  )
-
-;;; Buffer type constants
-
-(defconst bc--type-unsupported  'unsupported)
-(defconst bc--type-file         'file)
-(defconst bc--type-dired        'dired)
-(defconst bc--type-info         'info)
-(defconst bc--type-system       'system)
-
-;;; Constants
-(defconst bc--menu-buffer       "*Breadcrumb Bookmarks*")
-(defconst bc--file-magic        "WBC")
-(defconst bc--file-version      1)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -481,7 +488,6 @@ BOOKMARK is the bookmark to jump to, which has the form (FILENAME . POSITION)."
         (funcall switch-buffer-func (find-file-noselect filename))
         (goto-char position))
        ((eq type bc--type-info)
-        (require 'info)
         (Info-find-node filename (car position))
         (goto-char (cdr position)))
        ((eq type bc--type-system)
@@ -531,7 +537,7 @@ It's the position (point) for normal buffer and (info-node-name point) for Info 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; *Breadcrumb Bookmark* menu functions
 
-(defconst bc--menu-table-offset 7)
+
 
 (defun bc-menu-redraw ()
   "Redraw the breadcrumb bookmarks in the buffer named `*Breadcrumb Bookmarks*'."
@@ -746,7 +752,8 @@ The following commands are available.
   (when (and (not (null file))
              (file-readable-p file))
     (let ((loading-buffer (find-file-noselect file))
-          (bookmark-list))
+          (bookmark-list)
+	  (data-list))
       (setq data-list (with-current-buffer loading-buffer
                         (goto-char (point-min))
                         (read (current-buffer))))
@@ -770,7 +777,9 @@ The following commands are available.
 
 ;; Load from file on start up.
 (eval-after-load "breadcrumb"
-  '(bc-bookmarks-restore))
+  '(progn
+     (bc-bookmarks-restore)
+     (require 'info)))
 
 ;; Save to file on exit.
 (add-hook 'kill-emacs-hook 'bc-bookmarks-save)
@@ -822,79 +831,6 @@ If POS is nil, use current buffer location."
     (bury-buffer))
 )
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Testing and debugging
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;; Testing
-;;(eval-when-compile
-;; (bc-bookmarks-get 0)
-;; (bc-bookmarks-get 1)
-;; )
-
-;;; Debugging with C-xC-e
-(if nil
-    (progn
-      (global-set-key "\C-x\M-t"    'bc-dbgtest)
-      (bc-next)
-      (bc-previous)
-      (bc-clear)
-      (bc-list)
-      *bc-bookmark-just-added*
-      *bc-bookmarks*
-      *bc-current*
-      type
-      filename
-      position
-      (setq *bc-current* 0)
-      (1- (length *bc-bookmarks*))
-      (buffer-file-name)
-      (bc-get-buffer-filename bc--type-file)
-      (bc-get-buffer-position bc--type-file)
-      (bc-bookmarks-load-file bc-bookmark-file)
-      (bc-bookmarks-save-file *bc-bookmarks* bc-bookmark-file)
-      (bc-bookmarks-save)
-      (bc-bookmarks-restore)
-
-      (global-set-key [?\S-\040]  'bc-set)
-      (global-set-key "\M-j"      'bc-previous)
-      (global-set-key "\S-\M-j"   'bc-next)
-      (global-set-key [M-up]      'bc-local-previous)
-      (global-set-key [M-down]    'bc-local-next)
-      (global-set-key "\C-cj"     'bc-goto-current)
-      (global-set-key "\C-x\M-j"  'bc-list)
-      (global-set-key [C-f2]      'bc-set)
-      (global-set-key [f2]        'bc-previous)
-      (global-set-key [S-f2]      'bc-next)
-      (global-set-key [M-f2]      'bc-list)
-
-      (global-set-key [(shift space)] 'bc-set)
-      (global-set-key [(meta j)] 'bc-previous)
-      (global-set-key [(shift meta j)] 'bc-next)
-      (global-set-key [(meta up)] 'bc-local-previous)
-      (global-set-key [(meta down)] 'bc-local-next)
-      (global-set-key [(control c)(j)]     'bc-goto-current)
-      (global-set-key [(control x)(meta j)]  'bc-list)
-
-      (global-set-key [(control F2)]      'bc-set)
-      (global-set-key [(f2)]        'bc-previous)
-      (global-set-key [(shift f2)]      'bc-next)
-      (global-set-key [(meta f2)]      'bc-list)
-
-      )
-
-    (defun bc-dbgtest ()
-      (interactive)
-      ;(message (buffer-file-name))
-      ;(message (buffer-name))
-      (message (symbol-name major-mode))
-      )
-)
-
-;;
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (provide 'breadcrumb)
