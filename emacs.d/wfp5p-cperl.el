aa01711f893cc7503ba0043990e8e017300356a8
Bill Pemberton <wfp5p@virginia.edu>
2009-08-26 11:35:00 -0400

    more fixing up of cperl
    Man this is a broken mode.  It looks like advice is needed to really
    fix it.

--- emacs.d/wfp5p-cperl.el
+++ emacs.d/wfp5p-cperl.el
@@ -2,11 +2,16 @@
 
 (defun wfp5p-cperl-mode-hook ()
   (setq-default cperl-invalid-face 'default)
-  (setq-default cperl-hairy t)
   (setq-default cperl-electric-parens nil)
   (setq-default cperl-electric-parens-string nil)
+  (setq-default cperl-electric-keywords t)
+  (setq-default cperl-auto-newline t)
+  (setq-default cperl-autoindent-on-semi t)
+  (setq-default cperl-electric-linefeed t)
+  (setq-default cperl-hairy t)
   (turn-on-ws-trim)
   (cperl-set-style "BSD"))
+
 
 
 (add-hook 'cperl-mode-hook 'wfp5p-cperl-mode-hook)
