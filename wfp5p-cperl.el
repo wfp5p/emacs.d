9121e9247da8dc037533c4f7db847502852d65a4
Bill Pemberton <wfp5p@virginia.edu>
2010-10-04 12:17:48 -0400

    properly turn off some cperl-hairy options.

--- wfp5p-cperl.el
+++ wfp5p-cperl.el
@@ -7,14 +7,22 @@
 
 (ad-activate 'cperl-electric-paren)
 
+;; cperl-hariy will set these:
+;; cperl-font-lock
+;; cperl-electric-lbrace-space
+;; cperl-electric-parens
+;; cperl-electric-linefeed
+;; cperl-electric-keywords
+;; cperl-info-on-command-no-prompt
+;; cperl-clobber-lisp-bindings
+;; cperl-lazy-help-time
+
 (defun wfp5p-cperl-mode-hook ()
   (setq-default cperl-invalid-face 'default)
-  (setq-default cperl-electric-parens nil)
-  (setq-default cperl-electric-parens-string nil)
-  (setq-default cperl-electric-keywords t)
+  (setq-default cperl-electric-parens 'null)
+  (setq-default cperl-electric-keywords 'null)
   (setq-default cperl-auto-newline t)
   (setq-default cperl-autoindent-on-semi t)
-  (setq-default cperl-electric-linefeed t)
   (setq-default cperl-hairy t)
   (turn-on-ws-trim)
   (cperl-set-style "BSD"))
