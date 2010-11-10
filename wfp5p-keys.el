e43cb863757c9def1729402b0832980326dd529b
Bill Pemberton <wfp5p@virginia.edu>
2010-11-10 08:45:50 -0500

    Add the revert-all-buffers function
    Currently this is just commented out

--- wfp5p-keys.el
+++ wfp5p-keys.el
@@ -106,3 +106,16 @@ the next ARG lines after the ones already marked."
 (define-key global-map "\C-k" wfp5p-C-k-map)
 (provide 'wfp5p-keys)
 
+;; (defun revert-all-buffers ()
+;;           "Refreshes all open buffers from their respective files"
+;;           (interactive)
+;;           (let* ((list (buffer-list))
+;;                  (buffer (car list)))
+;;             (while buffer
+;;               (when (and (buffer-file-name buffer)
+;;                          (not (buffer-modified-p buffer)))
+;;                 (set-buffer buffer)
+;;                 (revert-buffer t t t))
+;;               (setq list (cdr list))
+;;               (setq buffer (car list))))
+;;           (message "Refreshed open files"))
