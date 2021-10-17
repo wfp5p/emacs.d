87bd4356a26702a99172948fc9cd0d33d2b8190b
Bill Pemberton <wfp5p@worldbroken.com>
2021-10-16 20:33:49 -0400

    Move custom settings into their own file
    This should make custom usage more obvious.

---
+++ custom.el
@@ -0,0 +1,49 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(gnus-novice-user nil)
+ '(inhibit-startup-screen t)
+ '(initial-frame-alist
+   '((height . 50)
+     (width . 80)
+     (height . 50)
+     (top + 50)
+     (tool-bar-lines)))
+ '(initial-scratch-message nil)
+ '(isearch-lazy-highlight nil)
+ '(make-backup-files nil)
+ '(mouse-yank-at-point t)
+ '(python-indent-guess-indent-offset nil)
+ '(remote-shell-program "ssh")
+ '(require-final-newline 'query)
+ '(search-highlight t)
+ '(shadow-info-file "~/.emacs.d/shadows")
+ '(shadow-todo-file "~/.emacs.d/shadow_todo")
+ '(show-trailing-whitespace t)
+ '(small-temporary-file-directory "/dev/shm")
+ '(tool-bar-mode nil nil (tool-bar))
+ '(tramp-default-method "ssh")
+ '(tramp-syntax 'simplified nil (tramp))
+ '(tramp-verbose 8)
+ '(truncate-lines t)
+ '(uniquify-buffer-name-style 'post-forward nil (uniquify)))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Input Mono" :foundry "FBI " :slant normal :weight normal :height 98 :width normal))))
+ '(cperl-array-face ((default nil) (nil nil)))
+ '(cperl-hash-face ((((class color) (background light)) nil)))
+ '(cperl-nonoverridable-face ((t (:foreground "blue"))))
+ '(font-lock-builtin-face ((((class color) (min-colors 88) (background light)) (:foreground "magenta"))))
+ '(font-lock-comment-face ((((class color) (min-colors 88) (background light)) (:foreground "red2"))))
+ '(font-lock-keyword-face ((((class color) (min-colors 8)) (:foreground "blue" :weight normal))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background light)) nil)))
+ '(font-lock-type-face ((((class color) (min-colors 88) (background light)) (:foreground "khaki4"))))
+ '(font-lock-variable-name-face ((((class color) (min-colors 88) (background light)) (:foreground "NavyBlue"))))
+ '(region ((t (:background "gray85" :foreground "black")))))
