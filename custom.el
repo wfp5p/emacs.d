dd9af85ce370ddc1f81c9bb0c3fcc707f10f242d
Bill Pemberton <wfp5p@worldbroken.com>
2021-10-20 11:35:22 -0400

    Fixup custom a bit

--- custom.el
+++ custom.el
@@ -13,8 +13,7 @@
  ;; If there is more than one, they won't work right.
  '(font-lock-builtin-face ((((class color) (min-colors 88) (background light)) (:foreground "magenta"))))
  '(font-lock-comment-face ((((class color) (min-colors 88) (background light)) (:foreground "red2"))))
- '(font-lock-keyword-face ((((class color) (min-colors 8)) (:foreground "blue" :weight normal))))
- '(font-lock-string-face ((((class color) (min-colors 88) (background light)) nil)))
+ '(font-lock-keyword-face ((((class color) (min-colors 88) (background light)) (:foreground "blue"))))
  '(font-lock-type-face ((((class color) (min-colors 88) (background light)) (:foreground "khaki4"))))
  '(font-lock-variable-name-face ((((class color) (min-colors 88) (background light)) (:foreground "NavyBlue"))))
  '(region ((t (:background "gray85" :foreground "black"))))
