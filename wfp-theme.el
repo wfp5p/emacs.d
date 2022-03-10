4786f7695635cb47d5d79e7a7d61ad21b57e13fe
Bill Pemberton <wfp5p@worldbroken.com>
2022-03-10 15:37:08 -0500

    Add perl colors to theme

--- wfp-theme.el
+++ wfp-theme.el
@@ -6,6 +6,9 @@
  '(font-lock-comment-face ((((class color) (min-colors 88) (background light)) (:foreground "red2"))))
  '(font-lock-keyword-face ((((class color) (min-colors 88) (background light)) (:foreground "blue"))))
  '(font-lock-variable-name-face ((((class color) (min-colors 88) (background light)) (:foreground "NavyBlue"))))
+ '(font-lock-string-face ((t (:foreground "dark magenta"))))
+ '(cperl-nonoverridable-face ((t (:foreground "dark green"))))
+ '(cperl-hash-face ((t (:foreground "Red"))))
  '(region ((t (:background "gray85" :foreground "black")))))
 
 (provide-theme 'wfp)
