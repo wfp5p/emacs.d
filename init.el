b0258cc29c025cd9c90e25774f99270d38a6806a
Bill Pemberton <wfp5p@worldbroken.com>
2024-07-26 12:35:15 -0400

    Switch to tree-sitter for python if it's available

--- init.el
+++ init.el
@@ -14,6 +14,9 @@
 
 (add-hook 'python-mode-hook #'wfp-python-mode-hook)
 
+(when (treesit-language-available-p 'python)
+  (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode))
+  (add-hook 'python-ts-mode-hook #'wfp-python-mode-hook))
 
 ;; Work this out, it's a lot of old
 ;; cc-mode
